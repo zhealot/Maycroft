ebf0d1ddfe37a79ad5cd00a2b9d1484207111f91
Tao Zhou <tao@allfields.co.nz>
2015-11-26 10:36:53 +1300

    bind template.docx dynamically

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Word = Microsoft.Office.Interop.Word;
 
@@ -29,23 +30,17 @@ namespace MaycroftOL
         private void button2_Click(object sender, EventArgs e)
         {
             this.Hide();
-            //create new word document for signature
-            Word.Application WdSignature = new Word.Application();
-            WdSignature.Visible = false;
-            //Word.Document SignatureDocu = WdSignature.Documents.Add();
-            var oSignatureObject = WdSignature.EmailOptions.EmailSignature;
-            var oSignatureEntry = oSignatureObject.EmailSignatureEntries;
-            
             //open template document
             Word.Application WdTemplate = new Word.Application();
             WdTemplate.Visible = false;
-            object oFilePath=@"C:\Users\Tao\OneDrive\Repo\maycroftol\Template.docx";
+            var oSignatureObject = WdTemplate.EmailOptions.EmailSignature;
+            var oSignatureEntry = oSignatureObject.EmailSignatureEntries;
             try
             {
+                object oFilePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase) + "\\Template.docx";
                 Word.Document TemplateDocu = WdTemplate.Documents.Open(ref oFilePath, ReadOnly: true, Visible: false);
                 //### replace key words here
                 string SigName = "AAAA";
-
                 System.Diagnostics.Debug.WriteLine(TemplateDocu.Name + " : " + TemplateDocu.Paragraphs[1].Range.Text);
                 var SigEntry = oSignatureEntry.Add(SigName, TemplateDocu.Content);
                 oSignatureObject.NewMessageSignature =SigName;
@@ -57,10 +52,8 @@ namespace MaycroftOL
                 System.Diagnostics.Debug.WriteLine(ex.Message);
             }
             WdTemplate.Quit();
-            WdSignature.Quit();
             //if (WdTemplate != null)
             //    System.Runtime.InteropServices.Marshal.ReleaseComObject(WdTemplate);
-
             GC.Collect();
         }
     }
